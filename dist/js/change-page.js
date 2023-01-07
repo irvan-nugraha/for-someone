316f8b2f7011b1574ce9e5328dd5c43016678faf
rivan-nugraha <piksi.irvan.18402043@gmail.com>
2023-01-08 01:21:00 +0700

    [First Commit]: Checking On GH-Pages

---
+++ dist/js/change-page.js
@@ -0,0 +1,107 @@
+function toSecondPage(){
+    var x = document.getElementById("no-1");
+    var y = document.getElementById("no-2");
+    if(x.className === "page active"){
+        x.className = "page"
+        x.className += " page-exit";
+        y.className += " active"
+    }else{
+        x.className = "page active"
+    }
+}
+
+function toThirdPage(){
+    var x = document.getElementById("no-2");
+    var y = document.getElementById("no-3");
+    if(x.className === "page active"){
+        x.className = "page"
+        x.className += " page-exit";
+        y.className += " active"
+    }else{
+        x.className = "page active"
+    }
+}
+
+function toFourthPage(){
+    var x = document.getElementById("no-3");
+    var y = document.getElementById("no-4");
+    if(x.className === "page active"){
+        x.className = "page"
+        x.className += " page-exit";
+        y.className += " active"
+    }else{
+        x.className = "page active"
+    }
+}
+
+function toFifthPage(){
+    var x = document.getElementById("no-4");
+    var y = document.getElementById("no-5");
+    if(x.className === "page active"){
+        x.className = "page"
+        x.className += " page-exit";
+        y.className += " active"
+    }else{
+        x.className = "page active"
+    }
+}
+
+function toSixthPage(){
+    var x = document.getElementById("no-5");
+    var y = document.getElementById("no-6");
+    if(x.className === "page active"){
+        x.className = "page"
+        x.className += " page-exit";
+        y.className += " active"
+    }else{
+        x.className = "page active"
+    }
+}
+
+function toSeventhPage(){
+    var x = document.getElementById("no-6");
+    var y = document.getElementById("no-7");
+    if(x.className === "page active"){
+        x.className = "page"
+        x.className += " page-exit";
+        y.className += " active"
+    }else{
+        x.className = "page active"
+    }
+}
+
+function toEigthPage(){
+    var x = document.getElementById("no-7");
+    var y = document.getElementById("no-8");
+    if(x.className === "page active"){
+        x.className = "page"
+        x.className += " page-exit";
+        y.className += " active"
+    }else{
+        x.className = "page active"
+    }
+}
+
+function toNinePage(){
+    var x = document.getElementById("no-8");
+    var y = document.getElementById("no-9");
+    if(x.className === "page active"){
+        x.className = "page"
+        x.className += " page-exit";
+        y.className += " active"
+    }else{
+        x.className = "page active"
+    }
+}
+
+function toTenthPage(){
+    var x = document.getElementById("no-9");
+    var y = document.getElementById("no-10");
+    if(x.className === "page active"){
+        x.className = "page"
+        x.className += " page-exit";
+        y.className += " active"
+    }else{
+        x.className = "page active"
+    }
+}
